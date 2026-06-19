0d1dc3097d7849e0bf23b1e64de904b480ca207f
Mihir Patil <mihirpatil7725@gmail.com>
2026-06-19 16:22:08 +0530

    Update bcrypt.js

--- Phase 6/libraries/bcrypt/bcrypt.js
+++ Phase 6/libraries/bcrypt/bcrypt.js
@@ -7,26 +7,26 @@ async function run() {
   const password = "mypassword123";
 
 
-  // STEP 1 — Hash the password
+  // Hash the password
   
   const hash = await bcrypt.hash(password, SALT_ROUNDS);
   console.log("Original :", password);
   console.log("Hashed   :", hash);
 
 
-  // STEP 2 — Compare correct password
+  // Compare correct password
 
   const correctMatch = await bcrypt.compare("mypassword123", hash);
   console.log("\nCorrect password match:", correctMatch);
 
 
-  // STEP 3 — Compare wrong password
+  // Compare wrong password
 
   const wrongMatch = await bcrypt.compare("wrongpassword", hash);
   console.log("Wrong password match  :", wrongMatch);
 
 
-  // STEP 4 — Same password, different hash
+  // Same password, different hash
 
   const hash2 = await bcrypt.hash(password, SALT_ROUNDS);
   console.log("\nHash 1:", hash);
